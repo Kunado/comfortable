065ddacaf55a508b10dd506317210bd3a8dadf60
naoki saruta <katsusand1@gmail.com>
2022-09-26 01:43:27 +0900

    Render table row's cells like original html element

--- src/components/Table/TableRow.tsx
+++ src/components/Table/TableRow.tsx
@@ -1,17 +1,18 @@
 import { FC } from "react";
+import parse from "html-react-parser";
 
 type TableRowProps = {
   row: string[]
 }
 
 export const TableRow: FC<TableRowProps> = ({ row }) => {
-  console.log(row)
   return (
     <tr>
     {
       row.map((cell, i) => {
+        const parsedCell = parse(cell)
         return (
-          <td key={i}>{cell}</td>
+          <td key={i}>{parsedCell}</td>
         )
       })
     }
